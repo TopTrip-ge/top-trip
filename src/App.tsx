4d87da374551d631fea27cbb7d20a019fe353a8d
Pavel Sokolov <pavlik.sokolov.2000@gmail.com>
2022-06-11 01:15:59 +0300

    booobee-7

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { FC } from "react";
 import { Routes } from "routes";
 import { MainThemeProvider } from "theme";
 import "./app.css";
-console.log("foofdfd");
+console.log("foofdfdffd");
 export const App: FC = () => (
   <div className="App">
     <MainThemeProvider>
