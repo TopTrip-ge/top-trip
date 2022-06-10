0b9754213913af38970f42bb3462b1a8daa06312
Pavel Sokolov <pavlik.sokolov.2000@gmail.com>
2022-06-10 10:35:32 +0300

    chore: Changed order of imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import reportWebVitals from "./reportWebVitals";
-import { App } from "./App";
-import "./index.css";
 
+import { App } from "./App";
 import { FirebaseProvider, firebaseInstances } from "./firebase";
+
+import reportWebVitals from "./reportWebVitals";
+
+import "./index.css";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!);
