98a25d3146e2ddbf94c542c3a90fc054bfdc7948
Pavel Sokolov <pavlik.sokolov.2000@gmail.com>
2022-06-11 19:55:14 +0300

    chore: Moved app

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
-import { App } from "./app";
+import { App } from "./components/app";
 import "./app-style.ts";
 
 const rootElement = document.getElementById("root");
