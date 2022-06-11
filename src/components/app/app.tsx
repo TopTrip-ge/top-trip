f57ba0470b8d1d0265c3e1f745f270e2a40c858d
Pavel Sokolov <pavlik.sokolov.2000@gmail.com>
2022-06-11 20:19:09 +0300

    chore: Changed firebase key

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,7 +1,7 @@
-import { GlobalStyle } from "app-style";
 import { FC } from "react";
 import { Routes } from "routes";
 import { MainThemeProvider } from "theme";
+import { GlobalStyle } from "./app-style";
 
 export const App: FC = () => (
   <MainThemeProvider>
--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { App } from "./components/app";
-import "./app-style.ts";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!);
