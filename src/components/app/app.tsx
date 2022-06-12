f7f32f16d9eb160b15a7658a51292842d3fe1c51
Pavel Sokolov <49731235+pavelsokolov21@users.noreply.github.com>
2022-06-12 23:13:37 +0300

    feat: Added firebase auto deploy (#15)

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,13 +1,11 @@
-import { GlobalStyle } from "app-style";
 import { FC } from "react";
 import { Routes } from "routes";
 import { MainThemeProvider } from "theme";
+import { GlobalStyle } from "./app-style";
 
 export const App: FC = () => (
   <MainThemeProvider>
     <GlobalStyle />
-    <div className="App">
-      <Routes />
-    </div>
+    <Routes />
   </MainThemeProvider>
 );
--- src/index.tsx
+++ src/index.tsx
@@ -2,16 +2,19 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
-import { App } from "./app";
-import "./app-style.ts";
+import { App } from "./components/app";
+import { PATHS } from "enums/paths";
+import { FirebaseProvider, firebaseInstances } from "./firebase";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!);
 
 root.render(
   <StrictMode>
-    <BrowserRouter basename={"/"}>
-      <App />
+    <BrowserRouter basename={PATHS.HOME}>
+      <FirebaseProvider firebaseInstances={firebaseInstances}>
+        <App />
+      </FirebaseProvider>
     </BrowserRouter>
   </StrictMode>
 );
