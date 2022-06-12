22de79fd273a475813a1262d9e666c42c8f992f3
Pavel Sokolov <pavlik.sokolov.2000@gmail.com>
2022-06-12 19:53:57 +0300

    bar

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,18 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import reportWebVitals from "./reportWebVitals";
 import { App } from "./components/app";
+import { PATHS } from "enums/paths";
+import { FirebaseProvider, firebaseInstances } from "./firebase";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!);
 
 root.render(
   <StrictMode>
-    <BrowserRouter basename={"/"}>
-      <App />
+    <BrowserRouter basename={PATHS.HOME}>
+      <FirebaseProvider firebaseInstances={firebaseInstances}>
+        <App />
+      </FirebaseProvider>
     </BrowserRouter>
   </StrictMode>
 );
