2c1c72d44266cbb1ce199e0a1490e4dc9aba973a
Pavel Sokolov <49731235+pavelsokolov21@users.noreply.github.com>
2022-06-13 19:21:18 +0300

    feat: Added pre-commit hook (#19)

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { PATHS } from "enums/paths";
 import reportWebVitals from "./reportWebVitals";
 import { App } from "./components/app";
-import { PATHS } from "enums/paths";
 import { FirebaseProvider, firebaseInstances } from "./firebase";
 
 const rootElement = document.getElementById("root");
+
 const root = createRoot(rootElement!);
 
 root.render(
