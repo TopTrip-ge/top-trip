658ff0f60f473aa643210e54f93ad27bcf51c197
Pavel Sokolov <pavlik.sokolov.2000@gmail.com>
2022-06-13 18:47:44 +0300

    chore: Prettiered

--- src/components/header/navbar/navbar.tsx
+++ src/components/header/navbar/navbar.tsx
@@ -1,22 +1,16 @@
 import { FC } from "react";
 import { Box, Tab, Tabs } from "@mui/material";
-import { StyledNavLink } from "./navbar-style";
 import { PATHS } from "enums/paths";
+import { StyledNavLink } from "./navbar-style";
 
 const NAVBAR_ITEMS = [
   <StyledNavLink to={PATHS.HOME}>Подбор тура</StyledNavLink>,
-  <StyledNavLink to={PATHS.POPULAR_DESTINATIONS}>
-    Популярные направления
-  </StyledNavLink>,
+  <StyledNavLink to={PATHS.POPULAR_DESTINATIONS}>Популярные направления</StyledNavLink>,
 ];
 
 export const Navbar: FC = () => (
   <Box sx={{ padding: "12px 0" }}>
-    <Tabs
-      component="nav"
-      aria-label="nav tabs"
-      sx={{ "[role='tablist']": { gap: 5 } }}
-    >
+    <Tabs component="nav" aria-label="nav tabs" sx={{ "[role='tablist']": { gap: 5 } }}>
       {NAVBAR_ITEMS.map((label) => (
         <Tab label={label} sx={{ p: 0, maxWidth: "100%" }} />
       ))}
