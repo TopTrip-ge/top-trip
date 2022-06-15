e099cc4242dd028448604e3bf0a266219ae04354
Pavel Sokolov <pavlik.sokolov.2000@gmail.com>
2022-06-15 18:50:18 +0300

    chore: Renamed hook

--- src/firebase-common/hooks/use-create-document.ts
+++ src/firebase-common/hooks/use-create-document.ts
@@ -2,7 +2,7 @@ import { collection, addDoc } from "firebase/firestore";
 import { useContext } from "react";
 import { FirebaseContext } from "../context";
 
-export const useSetDocument = <T extends object>(collectionName: string, data: T, additionalPath: string[] = []) => {
+export const useCreateDocument = <T extends object>(collectionName: string, data: T, additionalPath: string[] = []) => {
   const { firestore } = useContext(FirebaseContext);
 
   return async () => {
