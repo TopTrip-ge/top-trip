14d68aa2fe36dd8be1dd804f8a0ff22e649fb6bd
Pavel Sokolov <pavlik.sokolov.2000@gmail.com>
2022-06-16 01:22:36 +0300

    chore: Fixed imports

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,8 +1,7 @@
 import { FC } from "react";
 import { MainLayout } from "layouts/main";
 import { useHome } from "./hooks";
-import { Search } from "./components/search/search";
-import { WhyUs } from "./components";
+import { Search, WhyUs } from "./components";
 
 export const Home: FC = () => {
   const { searchProps } = useHome();
