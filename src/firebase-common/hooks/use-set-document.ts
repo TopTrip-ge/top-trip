0fc90c2bf564905a8d4559392e64ad866fdae44f
Pavel Sokolov <pavlik.sokolov.2000@gmail.com>
2022-06-15 18:44:07 +0300

    feat: Added useSetDoc hook

---
+++ src/firebase-common/hooks/use-set-document.ts
@@ -0,0 +1,11 @@
+import { collection, addDoc } from "firebase/firestore";
+import { useContext } from "react";
+import { FirebaseContext } from "../context";
+
+export const useSetDocument = <T extends object>(collectionName: string, data: T, additionalPath: string[] = []) => {
+  const { firestore } = useContext(FirebaseContext);
+
+  return async () => {
+    await addDoc(collection(firestore, collectionName, ...additionalPath), data);
+  };
+};
