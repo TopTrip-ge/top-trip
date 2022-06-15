00c1d4782647bb6869968521134d6cfb552548fe
Pavel Sokolov <pavlik.sokolov.2000@gmail.com>
2022-06-16 01:15:30 +0300

    feat: Added Why us section

---
+++ src/pages/home/components/why-us/interfaces/card.ts
@@ -0,0 +1,7 @@
+import { IconTypes } from "components/icon";
+
+export interface Card {
+  icon: IconTypes;
+  title: string;
+  description: string;
+}
---
+++ src/pages/home/components/why-us/why-us.tsx
@@ -0,0 +1,45 @@
+import { FC } from "react";
+import { Box, Card, CardContent, Container, Typography } from "@mui/material";
+import { Icon } from "components/icon";
+import { Section } from "components/section";
+import { Card as CardInfo } from "./interfaces";
+
+const CARDS: CardInfo[] = [
+  {
+    icon: "SearchOutlined",
+    title: "Lorem ipsum dolor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis magna sodales, posuere.",
+  },
+  {
+    icon: "SentimentSatisfiedAltOutlined",
+    title: "Lorem ipsum dolor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis magna sodales, posuere.",
+  },
+  {
+    icon: "CheckOutlined",
+    title: "Lorem ipsum dolor",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas quis magna sodales, posuere.",
+  },
+];
+
+export const WhyUs: FC = () => (
+  <Section>
+    <Container>
+      <Box sx={{ display: "flex", justifyContent: "space-between", gap: 2 }}>
+        {CARDS.map(({ description, icon, title }) => (
+          <Card sx={{ maxWidth: "350px", boxShadow: "none" }}>
+            <CardContent sx={{ display: "flex", flexDirection: "column", alignItems: "center", p: 0 }}>
+              <Icon name={icon} fontSize="large" />
+              <Typography fontWeight="fontWeightBold" variant="h5" sx={{ mt: 1 }}>
+                {title}
+              </Typography>
+              <Typography fontWeight="fontWeightMedium" variant="subtitle1" align="center" sx={{ mt: 2 }}>
+                {description}
+              </Typography>
+            </CardContent>
+          </Card>
+        ))}
+      </Box>
+    </Container>
+  </Section>
+);
