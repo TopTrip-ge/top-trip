a06279edd124f31aa26a0a51feb456aa1569c93e
Pavel Sokolov <49731235+pavelsokolov21@users.noreply.github.com>
2022-06-22 22:13:16 +0300

    feat: Added skeleton for the form (#32)

---
+++ src/hocs/with-skeleton/with-skeleton-constants.ts
@@ -0,0 +1,5 @@
+import { SxProps, Theme } from "@mui/material";
+
+export const SKELETON_STYLES: SxProps<Theme> = {
+  transform: "scale(1)",
+};
--- src/pages/home/components/search/search-constants.ts
+++ src/pages/home/components/search/search-constants.ts
@@ -9,3 +9,5 @@ export enum DESTINATIONS {
   SELECT_WHERE = "SELECT_WHERE",
   SELECT_FROM = "SELECT_FROM",
 }
+
+export const SKELETON_MIN_HEIGHT = "56px";
