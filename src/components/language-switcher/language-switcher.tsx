48ac1753f53fb661bdd0916caa9c9a6e5a3a916f
Pavel Sokolov <pavlik.sokolov.2000@gmail.com>
2022-06-25 17:58:23 +0300

    fear: Changed styles of switcher

--- src/components/language-switcher/language-switcher.tsx
+++ src/components/language-switcher/language-switcher.tsx
@@ -23,6 +23,11 @@ export const LanguageSwitcher: FC = () => {
         label={t("label.lang")}
         onChange={handleChange}
         value={i18n.language as LANGUAGES}
+        sx={{
+          [`#${LANG_SWITCHER_ID}`]: {
+            py: 1,
+          },
+        }}
       >
         {Object.entries(LANGUAGES).map(([key, value]) => (
           <MenuItem value={value}>{key}</MenuItem>
--- src/hooks/use-destinations.ts
+++ src/hooks/use-destinations.ts
@@ -4,8 +4,8 @@ import { LOCALIZATION_NAMESPACES } from "enums/localization";
 import { useMemo } from "react";
 
 export const useDestinations = () => {
-  const { t } = useTranslation([LOCALIZATION_NAMESPACES.DESTINATIONS]);
-  const destinations = useMemo(() => t("destinations") as string[], []);
+  const { t, i18n } = useTranslation([LOCALIZATION_NAMESPACES.DESTINATIONS]);
+  const destinations = useMemo(() => t("destinations") as string[], [i18n.language]);
 
   return destinations.map((name) => ({
     id: uniqid(),
