a6d2f6037c4d9d62d0afbac96ebf9d5c0f79948b
Pavel Sokolov <pavlik.sokolov.2000@gmail.com>
2022-06-25 20:07:04 +0300

    chore: Added flags and changed time to am/pm for EN

--- src/components/language-switcher/language-switcher.tsx
+++ src/components/language-switcher/language-switcher.tsx
@@ -2,7 +2,36 @@ import { FormControl, InputLabel, MenuItem, Select, SelectChangeEvent } from "@m
 import { FC } from "react";
 import { useTranslation } from "react-i18next";
 import { LANGUAGES } from "enums";
+import { makeFirebaseStoragePath } from "utils/make-firebase-storage-path";
 import { LANG_SWITCHER_ID, LANG_SWITCHER_LABEL_ID } from "./language-switcher-constants";
+import { StyledImg } from "./language-switcher-styles";
+
+const LANGUAGES_ITEM = [
+  {
+    label: (
+      <>
+        <StyledImg
+          src={makeFirebaseStoragePath("flags%2Fen-flag.svg?alt=media&token=8b26e54d-8ff7-44b2-bd08-2d1f60d5cc2c")}
+          alt="EN flag"
+        />
+        EN
+      </>
+    ),
+    value: LANGUAGES.EN,
+  },
+  {
+    label: (
+      <>
+        <StyledImg
+          src={makeFirebaseStoragePath("flags%2Fru-flag.svg?alt=media&token=fe1ec886-2b14-4418-bf21-260e61607b80")}
+          alt="RU flag"
+        />
+        RU
+      </>
+    ),
+    value: LANGUAGES.RU,
+  },
+];
 
 export const LanguageSwitcher: FC = () => {
   const { t, i18n } = useTranslation();
@@ -29,8 +58,10 @@ export const LanguageSwitcher: FC = () => {
           },
         }}
       >
-        {Object.entries(LANGUAGES).map(([key, value]) => (
-          <MenuItem value={value}>{key}</MenuItem>
+        {LANGUAGES_ITEM.map(({ label, value }) => (
+          <MenuItem key={value} value={value}>
+            {label}
+          </MenuItem>
         ))}
       </Select>
     </FormControl>
