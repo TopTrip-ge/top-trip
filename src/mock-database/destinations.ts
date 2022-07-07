a7c1d0aec3bb68a6d7e703e7bb36f734f7586eed
Dmitriy Aleshin <60598908+KoTuK2306@users.noreply.github.com>
2022-07-07 18:32:06 +0300

    feature/remove unnecessary direction (#56)

---
+++ src/mock-database/destinations.ts
@@ -0,0 +1,45 @@
+export const RUdestinations = [
+  {
+    label: "Абастумани",
+    id: "destination_1",
+  },
+  {
+    label: "Автовокзал Ортачала",
+    id: "destination_2",
+  },
+  {
+    label: "Агара",
+    id: "destination_3",
+  },
+  {
+    label: "Адигени",
+    id: "destination_4",
+  },
+  {
+    label: "Алгетское водохранилище",
+    id: "destination_5",
+  },
+];
+
+export const ENdestinations = [
+  {
+    label: "Abastumani",
+    id: "destination_1",
+  },
+  {
+    label: "Ortachala Bus Station",
+    id: "destination_2",
+  },
+  {
+    label: "Agara",
+    id: "destination_3",
+  },
+  {
+    label: "Adigeni",
+    id: "destination_4",
+  },
+  {
+    label: "Algeti Reservoir",
+    id: "destination_5",
+  },
+];
