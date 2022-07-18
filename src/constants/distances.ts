c43fe24fa3ac6f40af3abca6fd7111e5bdb99675
KoTuK2306 <dmitriy_aleshin_00@mail.ru>
2022-07-18 12:57:40 +0300

    feat: distances placed in independent file

---
+++ src/constants/distances.ts
@@ -0,0 +1,84 @@
+import { RUdestinations } from "mock-database/destinations";
+
+export const distances = [
+  {
+    id: `${RUdestinations[0].id} - ${RUdestinations[1].id}`,
+    distance: 237,
+  },
+  {
+    id: `${RUdestinations[0].id} - ${RUdestinations[2].id}`,
+    distance: 124,
+  },
+  {
+    id: `${RUdestinations[0].id} - ${RUdestinations[3].id}`,
+    distance: 22,
+  },
+  {
+    id: `${RUdestinations[0].id} - ${RUdestinations[4].id}`,
+    distance: 241,
+  },
+  {
+    id: `${RUdestinations[1].id} - ${RUdestinations[2].id}`,
+    distance: 112,
+  },
+  {
+    id: `${RUdestinations[1].id} - ${RUdestinations[3].id}`,
+    distance: 236,
+  },
+  {
+    id: `${RUdestinations[1].id} - ${RUdestinations[4].id}`,
+    distance: 46,
+  },
+  {
+    id: `${RUdestinations[2].id} - ${RUdestinations[3].id}`,
+    distance: 124,
+  },
+  {
+    id: `${RUdestinations[2].id} - ${RUdestinations[4].id}`,
+    distance: 158,
+  },
+  {
+    id: `${RUdestinations[3].id} - ${RUdestinations[4].id}`,
+    distance: 241,
+  },
+  {
+    id: `${RUdestinations[1].id} - ${RUdestinations[0].id}`,
+    distance: 237,
+  },
+  {
+    id: `${RUdestinations[2].id} - ${RUdestinations[0].id}`,
+    distance: 124,
+  },
+  {
+    id: `${RUdestinations[3].id} - ${RUdestinations[0].id}`,
+    distance: 22,
+  },
+  {
+    id: `${RUdestinations[4].id} - ${RUdestinations[0].id}`,
+    distance: 241,
+  },
+  {
+    id: `${RUdestinations[2].id} - ${RUdestinations[1].id}`,
+    distance: 112,
+  },
+  {
+    id: `${RUdestinations[3].id} - ${RUdestinations[1].id}`,
+    distance: 236,
+  },
+  {
+    id: `${RUdestinations[4].id} - ${RUdestinations[1].id}`,
+    distance: 46,
+  },
+  {
+    id: `${RUdestinations[3].id} - ${RUdestinations[2].id}`,
+    distance: 124,
+  },
+  {
+    id: `${RUdestinations[4].id} - ${RUdestinations[2].id}`,
+    distance: 158,
+  },
+  {
+    id: `${RUdestinations[4].id} - ${RUdestinations[3].id}`,
+    distance: 241,
+  },
+];
